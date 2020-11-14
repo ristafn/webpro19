b2cff6a46cbccf760f677aaf7ff418bd2289f4bb
ristafn <golemmite23@gmail.com>
2020-11-14 11:09:50 +0700

    add index and main.js

---
+++ praktikum05/js/main.js
@@ -0,0 +1,14 @@
+var test;
+test = 10
+
+var score = 90
+var score2 = 95
+var score3 = 100
+var score4 = 95
+
+var nilai = [90,95,100,95]
+alert(nilai[2])
+
+//const digunakan untuk variable yg statis(tidak berubah)
+//let digunakan untuk variable khusus dalam satu file(beda scope)
+//var digunakan untuk variable yg bisa dirubah
