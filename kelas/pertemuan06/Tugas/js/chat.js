97b14c1369695aa77d643207c26715036c654467
ristafn <golemmite23@gmail.com>
2020-11-16 09:34:40 +0700

    0110219070_Rista Fauji Nafsir_TI01_pertemuan06

---
+++ kelas/pertemuan06/Tugas/js/chat.js
@@ -0,0 +1,6 @@
+function kirim() {
+    var a = document.getElementById("forumChat").innerHTML;
+    var b = document.getElementById("chat").value;
+    document.write(a + "<br />" + "<br />");
+    document.write(b + "<br />");
+}
