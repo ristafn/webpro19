5492c7dc5b416b6dd30600d6197bbad292da98fe
ristafn <golemmite23@gmail.com>
2020-12-29 20:09:38 +0700

    kasih navbar di setiap hyperlink

---
+++ kelas/tugasBesar/js/searchbar.js
@@ -0,0 +1,8 @@
+var search = document.getElementById('search')
+var button1 = document.getElementById('button1')
+function searchbar() {
+    search.style.display = 'block'
+    buttonSearch.style.display = 'none'
+    buttonSearchFafa.style.display = 'block'
+
+}
