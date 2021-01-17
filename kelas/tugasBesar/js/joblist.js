3332f79d41f8cfc67a385efd8606bed64edf9a32
ristafn <golemmite23@gmail.com>
2021-01-17 23:25:25 +0700

    change id

--- kelas/tugasBesar/js/joblist.js
+++ kelas/tugasBesar/js/joblist.js
@@ -1,40 +1,28 @@
-function main(){
-    var kategori = document.getElementById('kategori')
-    var programmer1 = document.getElementById('programmer-1')
-    var programmer2 = document.getElementById('programmer-2')
-    var programmer3 = document.getElementById('programmer-3')
-    var network1 = document.getElementById('network-1')
-    var analyst1 = document.getElementById('analyst-1')
-
+function joblist(){
+    programmer1.style.display = 'none'
+    programmer2.style.display = 'none'
+    programmer3.style.display = 'none'
+    programmer4.style.display = 'none'
+    network1.style.display = 'none'
+    analyst1.style.display = 'none'
     if (kategori.value == 'all'){
         programmer1.style.display = 'block'
         programmer2.style.display = 'block'
         programmer3.style.display = 'block'
+        programmer4.style.display = 'block'
         network1.style.display = 'block'
         analyst1.style.display = 'block'
     } else if (kategori.value == 'programmer'){
         programmer1.style.display = 'block'
         programmer2.style.display = 'block'
         programmer3.style.display = 'block'
-        network1.style.display = 'none'
-        analyst1.style.display = 'none'
+        programmer4.style.display = 'block'
+        programmer5.style.display = 'block'
     } else if (kategori.value == 'network'){
-        programmer1.style.display = 'none'
-        programmer2.style.display = 'none'
-        programmer3.style.display = 'none'
         network1.style.display = 'block'
-        analyst1.style.display = 'none'
     } else if (kategori.value == 'analyst'){
-        programmer1.style.display = 'none'
-        programmer2.style.display = 'none'
-        programmer3.style.display = 'none'
-        network1.style.display = 'none'
         analyst1.style.display = 'block'
     } else if (kategori.value == 'dataScience'){
-        programmer1.style.display = 'none'
-        programmer2.style.display = 'none'
-        programmer3.style.display = 'none'
-        network1.style.display = 'none'
-        analyst1.style.display = 'none'
+        
     }
 }
