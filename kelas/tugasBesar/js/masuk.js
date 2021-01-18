9d76f88de02e2209f761b5fde74b37c293c12fe7
ristafn <golemmite23@gmail.com>
2021-01-18 22:32:37 +0700

    change path window.open

--- kelas/tugasBesar/js/masuk.js
+++ kelas/tugasBesar/js/masuk.js
@@ -1,8 +1,8 @@
 function masuk(){
     if(username.value == 'admin' && password.value == '100'){
-        window.open('admin_page/index.html','_self')
+        window.open('masuk_page/admin_page/index.html','_self')
     } else if(username.value == 'tamu' && password.value == '200'){
-        window.open('tamu_page/index.html','_self')
+        window.open('masuk_page/tamu_page/index.html','_self')
     } else{
         alert('Anda harus masuk dengan username dan kata sandi yang benar!')
     }
